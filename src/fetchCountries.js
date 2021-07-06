ef7f2553d16e0c3f087f68bc176cdb11aeaabfdb
tatidan <t.danyliuk@gmail.com>
2021-07-06 10:23:56 +0300

    corrections, pnotify added

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,10 @@
 import config from './config.json';
 
 const fetchCountries = (path = '/') => {
-  return fetch(config.url + path)
-    .then(response => {
-      if (response.status !== 404) return response.json();
-      else return Promise.reject('REQUEST ERROR');
-    })
-    .catch(error => {
-      errorHandler(error);
-    });
+  return fetch(config.url + path).then(response => {
+    if (response.status !== 404) return response.json();
+    else return Promise.reject('REQUEST ERROR');
+  });
 };
 
 export default fetchCountries;
-
-//https://restcountries.eu/rest/v2/name/{name}
-//fetchCountries('/france').then(data => console.log(data));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './sass/main.scss';
 import debounce from 'lodash.debounce';
+import { alert, info, error, defaultModules } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 import countryTpl from './country.hbs';
 import fetchCountries from './fetchCountries';
 
@@ -9,43 +12,38 @@ const responseRef = document.querySelector('.response');
 inputRef.addEventListener(
   'input',
   debounce(() => {
+    if (!inputRef.value) {
+      responseRef.innerHTML = '';
+      return;
+    }
     const requestTmp = `/${inputRef.value}`;
-    fetchCountries(requestTmp).then(data => {
-      console.log(data);
-      renderCountries(data);
-    });
+    fetchCountries(requestTmp).then(renderCountries).catch(errorHandler);
   }, 500),
 );
 
 function renderCountries(countries) {
-  if (!inputRef.value) {
-    return;
-  }
-
   if (countries.length === 1) {
     responseRef.innerHTML = countryTpl(countries);
   }
 
   if (countries.length >= 2 && countries.length <= 10) {
     const countriesList = countries.map(country => {
-      return `<li>${country.name}</li>`;
+      return `<ul class="country__list"><li>${country.name}</li></ul>`;
     });
     responseRef.innerHTML = countriesList.join('');
   }
 
   if (countries.length > 10) {
-    const message = `Слишком много стран содержат "${inputRef.value}". Уточните название для поиска страны.`;
-    responseRef.innerHTML = message;
+    alert({
+      text: `Слишком много стран содержат "${inputRef.value}". Уточните название для поиска страны.`,
+    });
+    responseRef.innerHTML = '';
   }
 }
 
-//Для оповещений используй плагин pnotify.
-
-// не работает обработчик ошибок:
-function errorHandler(error) {
-  const message = `Cтраны ${inputRef.value} не существует. Введите другое название.`;
-  responseRef.innerHTML = message;
+function errorHandler() {
+  alert({
+    text: `Cтраны ${inputRef.value} не существует. Введите другое название.`,
+  });
+  responseRef.innerHTML = '';
 }
-
-//// в languages массив, нужно в каждом элементе массива, объекте, взять значение ключа name
-////{...languages}.name
